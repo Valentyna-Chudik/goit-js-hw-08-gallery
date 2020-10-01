631b4b78ca96470b3a5fef39cb69ed2a6d4c2e8e
Valentyna Chudik <valentyna_chudik@ukr.net>
2020-10-01 15:05:14 +0300

    Markup (2nd meth. insertAdjacentHTM) done

--- js/gallery.js
+++ js/gallery.js
@@ -1,27 +1,38 @@
-import galleryItems from '/js/gallery-items.js';
-console.log(galleryItems);
+import galleryItems from './gallery-items.js';
 
-const elContainerRef = document.querySelector('.js-gallery');
+const galleryContainerRef = document.querySelector('.js-gallery');
 
-const makeGalleryEl = ({ preview, original, description }) => {
-  const galleryEl = document.createElement('li');
-  galleryEl.classList.add('gallery__item');
+// 1st METHOD (createElement)
 
-  const elLink = document.createElement('a');
-  elLink.classList.add('gallery__link');
-  elLink.href = original;
+// const makeGalleryItem = ({ preview, original, description }) => {
+//   const galleryItem = document.createElement('li');
+//   galleryItem.classList.add('gallery__item');
 
-  const galleryImg = document.createElement('img');
-  galleryImg.classList.add('gallery__image');
-  galleryImg.src = preview;
-  galleryImg.dataset.source = original;
-  galleryImg.alt = description;
+//   const galleryItemLink = document.createElement('a');
+//   galleryItemLink.classList.add('gallery__link');
+//   galleryItemLink.href = original;
 
-  galleryEl.append(elLink, galleryImg);
-  return galleryEl;
+//   const galleryImg = document.createElement('img');
+//   galleryImg.classList.add('gallery__image');
+//   galleryImg.src = preview;
+//   galleryImg.dataset.source = original;
+//   galleryImg.alt = description;
+
+//   galleryItem.append(elLink, galleryImg);
+//   return galleryEl;
+// };
+
+// const elements = galleryItems.map(makeGalleryEl);
+// console.log(elements);
+
+// galleryContainerRef.append(...elements);
+
+// 2nd METHOD (templating, insertAdjacentHTM)
+
+const makeGalleryMarkup = galleryItem => {
+  const { original, preview, description } = galleryItem;
+  return `<li class="gallery__item"><a class="gallery__link" href=${original}><img class="gallery__image" src=${preview} data-source=${original} alt=${description}/></a></li>`;
 };
 
-const elements = galleryItems.map(makeGalleryEl);
-console.log(elements);
-
-elContainerRef.append(...elements);
+const galleryMarkup = galleryItems.map(makeGalleryMarkup).join('');
+galleryContainerRef.insertAdjacentHTML('beforeend', galleryMarkup);
